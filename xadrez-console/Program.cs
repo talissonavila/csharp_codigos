5dd7577fd2cd9d056cc6d7b3ae714f316dae39dc
Talisson Avila <devtalisson@gmail.com>
2025-03-14 22:30:28 -0300

    tabuleiro inicial xadrez

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine(tab);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao {  get; set; }
+        public Cor cor { get; protected set; }
+        public int quantidadeDeMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca (Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tabuleiro = tabuleiro;
+            this.cor = cor;
+            quantidadeDeMovimentos = 0;
+        }
+    }
+}
