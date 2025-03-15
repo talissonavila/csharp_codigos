ec5649902cd8c1e1752745bc1f369b382ed768b1
Talisson Avila <devtalisson@gmail.com>
2025-03-15 13:33:57 -0300

    criando metodo em Tela para melhor visualização das peças pretas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -8,6 +8,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tabuleiro.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tabuleiro.colunas; j++)
                 {
                     if (tabuleiro.peca(i, j) == null)
@@ -15,10 +16,27 @@ namespace xadrez_console
                         Console.Write("- ");
                     } else
                     {
-                        Console.Write(tabuleiro.peca(i, j) + " ");
+                        imprimirPeca(tabuleiro.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h" );
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            if (peca.cor == Cor.Preta)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
